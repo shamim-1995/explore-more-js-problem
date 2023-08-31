dd91d19e573348640a8e6a2bda3f1db33a952047
shamim-1995 <sashamim987@gmail.com>
2023-08-31 14:55:26 +0600

    added cheapest js section

---
+++ cheapest.js
@@ -0,0 +1,32 @@
+const phones = [
+    {names:'Samsung', camera: 12, storage:'32gb', price:36000, color:'silver'},
+
+    {name:'Walton', camera:10, storage:'32gb', price:22000, color:'silver'},
+
+    {name:'iPhone', camera:10, storage:'32gb', price:82000, color:'silver'},
+
+    {name:'Xaomi', camera:10, storage:'32gb', price:52000, color:'silver'},
+
+    {name:'Oppo', camera:10, storage:'32gb', price:20000, color:'silver'},
+
+    {name:'Nokia', camera:10, storage:'32gb', price:44000, color:'silver'},
+
+    {name:'HTC', camera:10, storage:'32gb', price:62000, color:'silver'},
+]
+
+
+function cheapestPhone(phones){
+    let cheapest = phones[0];
+    for(let i = 0; i < phones.length; i++){
+        const phone = phones[i];
+        // console.log(phone);
+        if(phone.price < cheapest.price){
+            cheapest = phone;
+        }
+
+    }
+return cheapest;
+}
+
+const mySelection = cheapestPhone(phones);
+console.log(mySelection);
