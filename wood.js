e92c3e4f59948d21bce788e7256451007fd3c71b
shamim-1995 <sashamim987@gmail.com>
2023-08-31 13:20:59 +0600

    added wood.js section

---
+++ wood.js
@@ -0,0 +1,27 @@
+/*
+Fixed: per item wood requirements.
+1. chair --> 3cft.
+2. table -->10 cft.
+3. bed --> 50 cft.
+
+vary: quantity.
+*/
+
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+
+
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+
+    // console.log(chairQuantity, tableQuantity, bedQuantity);
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+    
+}
+const totalWood = woodCalculator(2, 2, 5);
+console.log('Total Wood Require:', totalWood);
